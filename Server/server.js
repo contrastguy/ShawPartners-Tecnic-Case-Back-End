030e40c90d4bfa8f47b80d0b8ef833cd66e07f22
Matheus Rodrigues <kgmatheus11@gmail.com>
2022-12-29 17:15:13 -0300

    Adding CORS

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = require("../index");
 const cors = require("cors");
+const module = require("./config");
 
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors(module))
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
