50ba2588cee1b0695a2b50a8cb5f06e79e9bb8dc
Matheus Rodrigues <kgmatheus11@gmail.com>
2022-12-29 00:11:48 -0300

    Adding the Router

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.use(express.json());
 const usuario = require("./router/routes")
 
 //definir a rota padrão com express
-app.use("/home" ,  usuario) // o /usuario é o a rota que você escreve depois do número da porta quando for chamar a API no Postman
+app.use("/" ,  usuario) // o /usuario é o a rota que você escreve depois do número da porta quando for chamar a API no Postman
+
 
 module.exports= app
 //OBS: endereço para chamar a API no Postman -> http://localhost:~número porta~/~nome da rota padrão~/
