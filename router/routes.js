d1f8edd79c45381b4b45a30bbe572d7a6067acf3
Matheus Rodrigues <kgmatheus11@gmail.com>
2022-12-29 19:46:57 -0300

    Adding CORS

--- router/routes.js
+++ router/routes.js
@@ -3,25 +3,28 @@ const express = require("express");
 const Router = express.Router();
 
 
-Router.get("/", function(req, res, next) {
-    res.send("Hello World") 
+Router.get("/", function(req, res, next) { 
     res.header("Access-Control-Allow-Origin", "*")
+    res.send("Hello World")
 })
 
 
 Router.get ("/users/:username/repos", function(req, res){
-    controller.useRepos.then((result)=>{res.send(result)
-        res.header("Access-Control-Allow-Origin", "*")})
+    controller.useRepos.then((result)=>{
+        res.header("Access-Control-Allow-Origin", "*")
+        res.send(result)})
 })
 
 Router.get ("/users/:username/hovercard", function(req, res){
-    controller.userDetails.then((result)=>{res.send(result)
-    res.header("Access-Control-Allow-Origin", "*")})
+    controller.userDetails.then((result)=>{
+    res.header("Access-Control-Allow-Origin", "*")
+    res.send(result)})
 })
 
 Router.get ("/users", function(req, res){
-    controller.userList.then((result)=>{res.send(result)
-    res.header("Access-Control-Allow-Origin", "*")})
+    controller.userList.then((result)=>{
+    res.header("Access-Control-Allow-Origin", "*")
+    res.send(result)})
 })
 
 
