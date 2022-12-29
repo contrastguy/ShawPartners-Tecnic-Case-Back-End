dec9ffd966596dacc254b2eeea7b4140b6ba6400
Matheus Rodrigues <kgmatheus11@gmail.com>
2022-12-29 16:10:44 -0300

    Adding CORS

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const app = express()
-const cors = require("cors");
 
 
-
-app.use(express.json());
-
-app.use(cors())
 //importar as rotas do routes.js
 const usuario = require("./router/routes")
 
 //definir a rota padrão com express
-app.use("/" ,  usuario , (req,res)=>{
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
-}) // o /usuario é o a rota que você escreve depois do número da porta quando for chamar a API no Postman
+app.use("/" ,  usuario ) // o /usuario é o a rota que você escreve depois do número da porta quando for chamar a API no Postman
 
 module.exports= app
 //OBS: endereço para chamar a API no Postman -> http://localhost:~número porta~/~nome da rota padrão~/
