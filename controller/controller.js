cf39a44f1375dd0e1cc3866b6893dbce3e7547c2
Matheus Rodrigues <kgmatheus11@gmail.com>
2022-12-30 12:14:19 -0300

    Updating API

--- controller/controller.js
+++ controller/controller.js
@@ -18,8 +18,7 @@ const tool = new octokit.Octokit({auth: process.env.TOKEN });
 //detalhes usuário
 async function getUserDetails(){
     try {
-      const user = await axios.get("https://api.github.com/users?username", {
-      });
+      const user = await axios.get("https://api.github.com/users?username");
       return user.data;
     } catch (error) {
       console.error(error);
