9feb8d577e2b9d175cbec94216cd0cc534fa7aa1
Matheus Rodrigues <kgmatheus11@gmail.com>
2022-12-30 01:05:51 -0300

    Updating a new function

--- controller/controller.js
+++ controller/controller.js
@@ -18,7 +18,7 @@ const tool = new octokit.Octokit({auth: process.env.TOKEN });
 //detalhes usuário
 async function getUserDetails(){
     try {
-      const user = await tool.request('GET /users/{username}',{username: 'USERNAME'})
+      const user = await tool.request('GET /users/{username}',{username: 'contrastguy'})
       return user.data;
     } catch (error) {
       console.error(error);
